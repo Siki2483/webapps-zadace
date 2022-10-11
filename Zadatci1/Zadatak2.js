52e443a2d476b528c91034b1d10798696c28e70c
unknown <josip6291@gmail.com>
2022-10-11 13:56:20 +0200

    Rijesen Zadatak 2

---
+++ Zadatci1/Zadatak2.js
@@ -0,0 +1,10 @@
+
+function provjera(x) {
+    if (x>=100 && x<=15000) {
+        console.log("taj broj se nalazi unutar [100,15000]")
+    }
+    else {
+        console.log(x+" -> "+x+" nije unutar [100,15000]")
+    }
+}
+provjera(53)
