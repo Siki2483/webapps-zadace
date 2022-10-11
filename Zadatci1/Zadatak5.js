f7001ab632e093447cb6fc94295aca2b40d669e7
unknown <josip6291@gmail.com>
2022-10-11 16:57:45 +0200

    Rijesen Zadatak 5

--- Zadatci1/Zadatak5.js
+++ Zadatci1/Zadatak5.js
@@ -1,11 +1,11 @@
-let x = "webb apps vjezbe";
+var x = "webb apps vjezbe"
 
 function cammelCase() {
-  let polje = x.split(" ");
-  for (let i = 1; i < polje.length; i++) {
-    polje[i] = polje[i][0].toUpperCase() + polje[i].substring(1);
+  var polje = x.split(" ")
+  for (var i = 1; i < polje.length; i++) {
+    polje[i] = polje[i][0].toUpperCase() + polje[i].substring(1)
   }
-  console.log(x+" -> "+polje.join(""));
+  console.log(x+" -> "+polje.join(""))
 }
 
 cammelCase();
